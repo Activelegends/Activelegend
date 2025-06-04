2f553a587ab1ac8492c2a0b69fcbd532fd4bad34
Active Legends Team <active.legendss@gmail.com>
2025-06-05 02:27:44 +0330

    Update UserMenu.tsx

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -31,7 +31,7 @@ export default function UserMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-left rounded-md bg-black border border-white/10 shadow-lg focus:outline-none">
+        <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-left rounded-md bg-black border border-white/10 shadow-lg focus:outline-none">
           <div className="p-2">
             <div className="px-3 py-2 text-sm text-gray-300">
               {user.email}
