611fe742324bde15add336160a3b7077d4bf9399
Active Legends Team <active.legendss@gmail.com>
2025-06-09 09:19:35 +0330

    تغییر سفید به سیاه 6

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -78,8 +78,8 @@ export const Games: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 pt-24">
-      <div className="container mx-auto px-4">
+    <div className="min-h-screen bg-black pt-24">
+      <div className="container mx-auto px-4 md:px-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
           {games.map((game) => (
             <GameCard key={game.id} game={game} />
