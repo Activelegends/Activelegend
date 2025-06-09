1ca3434bdf20dbd6f3da6e8683e4e25de5165630
Active Legends Team <active.legendss@gmail.com>
2025-06-09 08:53:55 +0330

    تیره 55

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -112,7 +112,7 @@ export default function TermsAndConditionsPage() {
               onChange={(e) => setIsAgreed(e.target.checked)}
               className="w-5 h-5 text-[#F4B744] border-gray-300 rounded focus:ring-[#F4B744]"
             />
-            <label htmlFor="agree" className="mr-3 text-gray-700 text-lg">
+            <label htmlFor="agree" className="mr-3 text-gray-900 text-lg font-medium">
               من قوانین و مقررات بالا را مطالعه کرده و با آن‌ها موافقم
             </label>
           </div>
@@ -140,7 +140,7 @@ export default function TermsAndConditionsPage() {
           </button>
 
           {!hasScrolledToBottom && (
-            <p className="text-yellow-600 text-center mt-4 flex items-center justify-center">
+            <p className="text-yellow-700 text-center mt-4 flex items-center justify-center font-medium">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
