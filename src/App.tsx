4e549efe035fc302d70d3052487f5714c9f5199a
Active Legends Team <active.legendss@gmail.com>
2025-07-23 10:04:24 +0330

    تبدیل # به سایت قابل جستجو در گوگل

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AnimatedBackground from './components/AnimatedBackground';
